7ba4723c64a44ccf1ae0ad0f7471b3aa9a817db8
koutaro <julianseed@gmail.com>
2016-10-30 20:29:43 +0900

    課題提出前の状態

---
+++ app/src/main/java/jp/techacademy/yasuhiko/tokushima/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.yasuhiko.tokushima.qa_app;
+
+/**
+ * Created by tokushima on 2016/10/23.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users";         // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH="contents";     // Firebaseに質問を保存するパス
+    public static final String AnswersPATH="answers";       // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name";            // Preferenceに表示名を保存する時のキー
+}
